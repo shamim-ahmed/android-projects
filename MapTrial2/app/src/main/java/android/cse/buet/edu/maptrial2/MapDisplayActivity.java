52490c670e25907ab448558d0b9c9cf9a4a09d3d
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-01-04 20:06:29 +1100

    now the marker on Google map is added based on user input

--- MapTrial2/app/src/main/java/android/cse/buet/edu/maptrial2/MapDisplayActivity.java
+++ MapTrial2/app/src/main/java/android/cse/buet/edu/maptrial2/MapDisplayActivity.java
@@ -1,12 +1,17 @@
 package android.cse.buet.edu.maptrial2;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 public class MapDisplayActivity extends Activity {
   @Override
@@ -20,7 +25,28 @@ public class MapDisplayActivity extends Activity {
     GoogleMap aMap = mapView.getMap();
 
     if (aMap != null) {
-      aMap.addMarker(new MarkerOptions().position(new LatLng(23, 90)).title("Dhaka, My Love !"));
+      Intent intent = getIntent();
+      String location = intent.getStringExtra("location");
+
+      if (location != null && !location.trim().equals("")) {
+        updateMap(aMap, location);
+      }
+    }
+  }
+
+  private void updateMap(GoogleMap map, String location) {
+    Geocoder geoCoder = new Geocoder(this);
+
+    try {
+      List<Address> addressList = geoCoder.getFromLocationName(location, 1);
+
+      if (addressList != null && addressList.size() > 0) {
+        Address address = addressList.get(0);
+        String description = String.format("%s, %s", location, address.getCountryName());
+        map.addMarker(new MarkerOptions().position(new LatLng(address.getLatitude(), address.getLongitude())).title(description));
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace(System.err);
     }
   }
 }
