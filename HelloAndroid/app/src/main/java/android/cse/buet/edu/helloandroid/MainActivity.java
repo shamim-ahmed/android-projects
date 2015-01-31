0b969553d8bc7f68a0433795d9c6bc5c8d2187e2
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-01-31 13:45:46 +1100

    reformatted java code

--- HelloAndroid/app/src/main/java/android/cse/buet/edu/helloandroid/MainActivity.java
+++ HelloAndroid/app/src/main/java/android/cse/buet/edu/helloandroid/MainActivity.java
@@ -11,11 +11,11 @@ import android.widget.TextView;
  */
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView textView = new TextView(this);
-        textView.setText("This is a basic android app");
-        setContentView(textView);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    TextView textView = new TextView(this);
+    textView.setText("This is a basic android app");
+    setContentView(textView);
+  }
 }
