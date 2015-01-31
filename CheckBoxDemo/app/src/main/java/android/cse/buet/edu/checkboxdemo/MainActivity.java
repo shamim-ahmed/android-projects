32da00ee5487f695f2f9c3bc54c439daeff0b799
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2015-01-31 20:40:59 +1100

    added a project that demonstrates how to use check boxes

---
+++ CheckBoxDemo/app/src/main/java/android/cse/buet/edu/checkboxdemo/MainActivity.java
@@ -0,0 +1,44 @@
+package android.cse.buet.edu.checkboxdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    final CheckBox iosCheckBox = (CheckBox) findViewById(R.id.iosCheckBox);
+    final CheckBox androidCheckBox = (CheckBox) findViewById(R.id.androidCheckBox);
+    final CheckBox windowsCheckBox = (CheckBox) findViewById(R.id.windowsCheckBox);
+    final Button doneButton = (Button) findViewById(R.id.doneButton);
+
+    iosCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+          Toast.makeText(MainActivity.this, "Try Android, Bro !", Toast.LENGTH_LONG).show();
+        }
+      }
+    });
+
+    doneButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        StringBuilder resultBuilder = new StringBuilder();
+
+        resultBuilder.append(String.format("iOS selected ? %b%n", iosCheckBox.isChecked()))
+            .append(String.format("Android selected ? %b%n", androidCheckBox.isChecked()))
+            .append(String.format("Windows selected ? %b%n", windowsCheckBox.isChecked()));
+        Toast.makeText(MainActivity.this, resultBuilder.toString(), Toast.LENGTH_LONG).show();
+      }
+    });
+  }
+}
