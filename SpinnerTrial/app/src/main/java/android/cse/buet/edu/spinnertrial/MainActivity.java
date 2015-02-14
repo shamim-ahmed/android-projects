f4a6cd75663d3a343dc263d08fd29c2ff56c5bd5
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-02-14 14:10:17 +1100

    added a project that demonstrates basic Spinner functionality

---
+++ SpinnerTrial/app/src/main/java/android/cse/buet/edu/spinnertrial/MainActivity.java
@@ -0,0 +1,41 @@
+package android.cse.buet.edu.spinnertrial;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+public class MainActivity extends Activity {
+
+  private static final String[] MONTH_NAMES = {"January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" };
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    final Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+    final Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+    Button okButton = (Button) findViewById(R.id.okButton);
+
+    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item,
+        Arrays.asList(MONTH_NAMES));
+    spinner2.setAdapter(arrayAdapter);
+
+    okButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(MainActivity.this,
+            String.format("country = %s and month = %s", spinner1.getSelectedItem(), spinner2.getSelectedItem()),
+            Toast.LENGTH_LONG).show();
+      }
+    });
+
+  }
+}
